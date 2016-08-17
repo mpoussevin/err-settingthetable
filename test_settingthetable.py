f933d25d8f30b66118faf2c4c74334575ec391cb
Mickaël Poussevin <mickael.poussevin@gmail.com>
2016-08-17 17:10:41 +0200

    Added unit tests.

---
+++ test_settingthetable.py
@@ -0,0 +1,20 @@
+"""Tests for the SettingTheTable module."""
+import os
+from errbot.backends.test import testbot
+
+
+class TestSettingTheTable(object):
+    extra_plugin_dir = "."
+
+    def test_look_out_for_table_flips(self, testbot):
+        """Test the module's function setting tables."""
+        testbot.push_message("(╯°□°）╯︵ ┻━┻")
+        assert "┬──┬ ノ( ゜-゜ノ)" in testbot.pop_message()
+        testbot.push_message("(╯°□°）╯︵ ┻━┻ ┻━┻")
+        assert "┬──┬ ┬──┬ ノ( ゜-゜ノ)" in testbot.pop_message()
+        testbot.push_message("(╯°□°）╯︵ ┻━┻┻━┻")
+        assert "┬──┬ ┬──┬ ノ( ゜-゜ノ)" in testbot.pop_message()
+        testbot.push_message("(╯°□°）╯︵ ┻━┻ ┻━┻ ┻━┻")
+        assert "┬──┬ ┬──┬ ┬──┬ ノ( ゜-゜ノ)" in testbot.pop_message()
+        testbot.push_message("(╯°□°）╯︵ ┻━┻ ┻━┻ ┻━┻ ┻━┻")
+        assert "┬──┬ ┬──┬ ┬──┬ ┬──┬ ノ( ゜-゜ノ)" in testbot.pop_message()
